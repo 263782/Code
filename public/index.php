812cf7fddd078a949c52579047538009c23c93e0
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-05-20 20:45:54 +0200

    added the sort of add boxes (tarieven, RIjbewijs and auto)

--- public/index.php
+++ public/index.php
@@ -10,14 +10,49 @@
     <div class="welkom">
         <p>WELKOM BIJ</p>
         <h1>STAL RIJOPLEIDING</h1>
-        <form action="http://google.com">
-                <input class="welkom__button" type="submit" value="MEER INFORMATIE" />
+        <form action="http://">
+            <input class="welkom__button" type="submit" value="MEER INFORMATIE" />
         </form>
     </div>
-    <div>
-        <img src="assets/images/background.png" class="stripe" alt="background">
+    <div class="uitleg"> 
+        <h1>Welkom op de website van Stal Rijopleiding!</h1> <br>
+        <p>Je autorijbewijs halen? Dan ben je bij ons aan het juiste adres. <br>
+        Wij geven les in <i>Zaandam, Koog aan de Zaan, Zaandijk, Westzaan, Assendelft, Wormerveer, Krommenie,</i> <br>
+        maar ook in <i>Wormer, Oostzaan, Landsmeer en Uitgeest</i>. Oftewel, de gehele Zaanstreek en omstreken!</p><br>
+        <br><p>Wij leiden je op tot een <b>veilig</b> en <b>zelfstandig</b> automobilist. De cursus verloopt volgens een gestructureerde <br>
+        opbouw van makkelijk naar moeilijk. <br>
+        Jouw leervorderingen houden we bij op een instructiekaart. Na afloop van de les bespreken we het onderwerp <br>
+        van de volgende rijles, zodat jij je alvast goed kan voorbereiden! .</p> <br>
+        <p>Heb je last van <b><i>faalangst?</i></b> Dan zul je je zeker prettig voelen bij onze lesaanpak. <br>
+        We hebben tevens een ruime ervaring in het lesgeven <br>
+        aan mensen met een vorm van <b><i>autisme</i></b>.</p> <br>
+        <p><i>Een proefles aanvragen?</i></p> <br>
+        <form action="http://">
+            <input class="uitleg__button" type="submit" value="KLIK DAN HIER"/>
+        </form> 
     </div>
-    
+    <div class="meerinfo">
+            <div class="blok">
+                <h1>RIJBEWIJS HALEN</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14 17H4v2h10v-2zm6-8H4v2h16V9zM4 15h16v-2H4v2zM4 5v2h16V5H4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>                <form action="http://">
+                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
+                </form> 
+            </div>
+
+            <div class="blok">
+                <h1>DE AUTO</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>                <form action="http://">
+                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
+                </form> 
+            </div>
+
+            <div class="blok">
+                <h1>TARIEVEN</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 18.5c-2.51 0-4.68-1.42-5.76-3.5H15v-2H8.58c-.05-.33-.08-.66-.08-1s.03-.67.08-1H15V9H9.24C10.32 6.92 12.5 5.5 15 5.5c1.61 0 3.09.59 4.23 1.57L21 5.3C19.41 3.87 17.3 3 15 3c-3.92 0-7.24 2.51-8.48 6H3v2h3.06c-.04.33-.06.66-.06 1 0 .34.02.67.06 1H3v2h3.52c1.24 3.49 4.56 6 8.48 6 2.31 0 4.41-.87 6-2.3l-1.78-1.77c-1.13.98-2.6 1.57-4.22 1.57z"/><path fill="none" d="M0 0h24v24H0z"/></svg>                <form action="http://">
+                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
+                </form> 
+            </div>
+    </div>
 </div>
 </body> 
 </html>
