9429b3155fe703c7d8c3957af024f769ab32b92e
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-05-20 19:56:05 +0200

    small piece of the home page

--- public/index.php
+++ public/index.php
@@ -6,5 +6,18 @@
 </head>
 <body>
 <?php include 'includes/nav.php'; ?>
-</body>
+<div class="wrapper">
+    <div class="welkom">
+        <p>WELKOM BIJ</p>
+        <h1>STAL RIJOPLEIDING</h1>
+        <form action="http://google.com">
+                <input class="welkom__button" type="submit" value="MEER INFORMATIE" />
+        </form>
+    </div>
+    <div>
+        <img src="assets/images/background.png" class="stripe" alt="background">
+    </div>
+    
+</div>
+</body> 
 </html>
