fbf4dfcd5effe3706da23f93108372aacbb7c301
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-05-22 18:47:44 +0200

    finnished opleiding.php

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -10,11 +10,11 @@
         <h2>CONTACT</h2>
         <a href="mailto:info@stalrijopleiding.nl">EMAIL</a>
         <a href="tel:+06-26881416">TELEFOON</a>
-        <a href="https://www.kvk.nl/zoeken/?source=all&q=stal%20rijopleiding&start=0&site=kvk2014">KVK</a>
+        <a target="_blank" href="https://www.kvk.nl/zoeken/?source=all&q=stal%20rijopleiding&start=0&site=kvk2014">KVK</a>
     </div>
     <div class="footer_rechts">
         <h2>EXTERNE INFORMATIE</h2>
-        <p>Aanvragen van een proefles ? <br> (gratis en vrijblijvend) Bel of <br> WhatsApp naar <a href="tel:+06-26881416">0626881416</a> of <br> <a href="mailto:info@stalrijopleiding.nl"> info@stalrijopleiding.nl</a>i</p>
+        <p>Aanvragen van een proefles ? <br> (gratis en vrijblijvend) Bel of <br> WhatsApp naar <a href="tel:+06-26881416">0626881416</a> of <br> <a href="mailto:info@stalrijopleiding.nl"> info@stalrijopleiding.nl</a></p>
     </div>
 </div>
     <div class="footer_credits">
