e1eb1d90cd93b8627725b1a01e125237b894a651
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-05-27 16:56:59 +0200

    started working on links.php

---
+++ public/links.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/links.css">
+</head>
+<body>
+    <?php include 'includes/nav.php' ?>
+<div class="wrapper">
+    <H1>LINKS</H1>
+    <div class="videos">
+        <div class="vid">
+            <h3>BEN JIJ GEZOND GENOEG OM JE RIJBEWIJS TE HALEN?</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/a-AIu1lgy-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div> 
+        <div class="vid">
+            <h3>HOE RESERVEER IK EEN THEORIE EXAMEN?</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/36IxNKU-uuk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>  
+    </div>
+    <div class="video">
+        <div class="vi">
+            <h3>ZO GAAT HET BIJ HET CBR THEORIE-EXAMEN</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/J6ONRzRDH7M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>        
+        <div class="vi">
+            <h3>ZO GAAT HET BIJ HET CBR PRAKTIJK EXAMEN</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/90DRSSl4QLw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+    <?php include 'includes/footer.php' ?>
+</body>
+</html>
