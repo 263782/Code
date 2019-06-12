f4fa56a590da794efed9282d391c5a0904578124
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-06-12 20:34:29 +0200

    copied and moved all old files to the MVC structure

--- public/index.php
+++ public/index.php
@@ -1,138 +1,95 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Stal Rijopleiding</title>
-    
-</head>
-<body>
-<?php include 'includes/nav.php'; ?>
-<div class="wrapper">
-    <div class="welkom">
-        <p>STAL RIJOPLEIDING</p>
-        <h1>DE SLEUTEL NAAR VEILIG EN ZELFSTANDIG AUTORIJDEN!</h1>
-    </div>
-    <div class="uitleg"> 
-        <h1>Welkom op de website van Stal Rijopleiding!</h1> <br>
-        <p>Je autorijbewijs halen? Dan ben je bij ons aan het juiste adres. <br>
-        Wij geven les in <i>Zaandam, Koog aan de Zaan, Zaandijk, Westzaan, Assendelft, Wormerveer, Krommenie,</i> <br>
-        maar ook in <i>Wormer, Oostzaan, Landsmeer en Uitgeest</i>. Oftewel, de gehele Zaanstreek en omstreken!</p><br>
-        <br><p>Wij leiden je op tot een <b>veilig</b> en <b>zelfstandig</b> automobilist. De cursus verloopt volgens een gestructureerde <br>
-        opbouw van makkelijk naar moeilijk. <br>
-        Jouw leervorderingen houden we bij op een instructiekaart. Na afloop van de les bespreken we het onderwerp <br>
-        van de volgende rijles, zodat jij je alvast goed kan voorbereiden! .</p> <br>
-        <p>Heb je last van <b><i>faalangst?</i></b> Dan zul je je zeker prettig voelen bij onze lesaanpak. <br>
-        We hebben tevens een ruime ervaring in het lesgeven <br>
-        aan mensen met een vorm van <b><i>autisme</i></b>.</p> <br>
-        <p><i>Een proefles aanvragen?</i></p> <br>
-        <form action="http://">
-            <input class="uitleg__button" type="submit" value="KLIK DAN HIER"/>
-        </form>  
-    </div>
-    <div class="meerinfo">
-            <div class="blok">
-                <h2>DE OPLEIDING</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14 17H4v2h20v-2zm6-8H4v2h16V9zM4 15h16v-2H4v2zM4 5v2h16V5H4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>                <form action="http://">
-                    <form action="http://26378.hosts2.ma-cloud.nl/code/public/opleiding.php">
-                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
-                </form> 
-            </div>
+<?php
+/**
+ * Dit is de Front controller
+ * ALLE requests die naar de webserver worden gedaan worden via de .htaccess naar dit script gestuurd.
+ * Zo voorkom je dat je heel veel PHP bestanden hebt met dubbele code en logica.
+ *
+ * Alle urls hebben de volgende structuur: http://localhost/<controller>/<actie>/<optionele parameters>
+ *
+ * Bijv: http://localhost/page/show/about-us
+ * (roept de show functie aan in de page controller met de parameter 'about-us')
+ *
+ * controller: page
+ * actie: show
+ * parameter: about-us
+ *
+ * De Front Controller (dit bestand) doorloopt elke keer de volgende procedure:
+ *
+ * 1. Inspecteert de URL die wordt opgevraagd
+ * 2. Zoekt op in een "routing" tabel of hij deze URL of dit URL patroon kent
+ * 3. Zo niet, dan is het een 404 oagina, want de website kent deze url niet
+ * 4. Als de url wel wordt herkend en er een "route" is naar de juiste code dan:
+ *  A. Wordt de juiste controller actie aangeroepen
+ *  B. De controller krijgt alle gegevens door (de url, $_GET, $_POST, $_FILES etc)
+ *  C. De controller haalt eventueel gegevens op via de Model laag (database queries e.d.)
+ *  D. De controller geeft de gegevens aan de juiste view
+ *  E. De view toont de gegevens op de juiste manier (met behulp van een foreach, if, switch e.d.)
+ *  F. De complete view (met data) wordt door de controller teruggestuurd naar de gebruiker (dit is de Response)
+ *
+ */
 
-            <div class="blok">
-                <h2>DE AUTO</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>                <form action="http://">
-                <form action="http://26378.hosts2.ma-cloud.nl/code/public/auto.php">
-                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
-                </form> 
-            </div>
+/**
+ * Download de kant en klare "router" van de AltoRouter website en zet het bestand AltoRouter.php in je private includes map
+ * https://github.com/dannyvankooten/AltoRouter/zipball/master
+ *
+ * Lees vooral de documentatie op http://altorouter.com/
+ *
+ */
+require '../private/includes/AltoRouter.php';
 
-            <div class="blok">
-                <h2>TARIEVEN</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 18.5c-2.51 0-4.68-1.42-5.76-3.5H15v-2H8.58c-.05-.33-.08-.66-.08-1s.03-.67.08-1H15V9H9.24C10.32 6.92 12.5 5.5 15 5.5c1.61 0 3.09.59 4.23 1.57L21 5.3C19.41 3.87 17.3 3 15 3c-3.92 0-7.24 2.51-8.48 6H3v2h3.06c-.04.33-.06.66-.06 1 0 .34.02.67.06 1H3v2h3.52c1.24 3.49 4.56 6 8.48 6 2.31 0 4.41-.87 6-2.3l-1.78-1.77c-1.13.98-2.6 1.57-4.22 1.57z"/><path fill="none" d="M0 0h24v24H0z"/></svg>                <form action="http://">
-                <form action="http://26378.hosts2.ma-cloud.nl/code/public/tarieven.php">
-                    <input class="uitleg__button" type="submit" value="MEER INFORMATIE"/>
-                </form> 
-            </div>
-    </div>
-    <div class="background2">
-        <div class="background-fix">
-       
-        <div class="posts">
-                <svg xmlns="http://www.w3.org/2000/svg" class="svg" width="100px" height="100px" viewBox="0 0 24 24"><path d="M14 7l-5 5 5 5V7z"/><path fill="none" d="M24 0v24H0V0h24z"/></svg>
-                <div class="block">
-                    <div class="block_top">
-                        <img src="assets/images/LOGO_STAL.png" alt="logo">
-                        <p>STAL RIJOPLEIDING</p>
-                    </div>
-                    <div class="block_date">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="block_svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
-                        <p>22 - 05 - 2019</p>
-                    </div>
-                    <div class="block_text">
-                    Onze nieuwe Facebook actie: Voor slechts 165,00 euro per maand je rijbewijs halen? <br> Nu kan het!
-                    Neem een 35 uren rijlessenpakket inclusief CBR tussentijdse toets en CBR praktijkexamen en betaal in 12 termijnen. <br>
-                    Zo houden we de rijlessen voor iedereen toegankelijk! <br>
-                    </div>
-                    <img src="assets/images/auto.jpeg" alt="auto" class="post">
-                </div>
+/**
+ * Verder willen we nog wat andere zaken instellen en goed zetten
+ * Dit staat in het bestand private/includes/init.php
+ */
+$CONFIG = require '../private/includes/config.php';
+require '../private/includes/init.php';
 
-                <div class="block">
-                    <div class="block_top">
-                        <img src="assets/images/LOGO_STAL.png" alt="logo">
-                        <p>STAL RIJOPLEIDING</p>
-                    </div>
-                    <div class="block_date">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="block_svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
-                        <p>22 - 05 - 2019</p>
-                    </div>
-                    <div class="block_text">
-                    Onze nieuwe Facebook actie: Voor slechts 165,00 euro per maand je rijbewijs halen? <br> Nu kan het!
-                    Neem een 35 uren rijlessenpakket inclusief CBR tussentijdse toets en CBR praktijkexamen en betaal in 12 termijnen. <br>
-                    Zo houden we de rijlessen voor iedereen toegankelijk! <br>
-                    </div>
-                    <img src="assets/images/auto.jpeg" alt="auto" class="post">
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="svg" width="100px" height="100px" viewBox="0 0 24 24"><path d="M10 17l5-5-5-5v10z"/><path fill="none" d="M0 24V0h24v24H0z"/></svg>
-            </div>
+$router = new AltoRouter();
 
-            <div class="extra-info">
-                <div class="info__blok">
-                        <h2>ENGLISH DRIVING LESSONS</h2>
-                        <p> We also provide English driving lessons. These will be given by Vincent. <br>
-                        A lesson package will include not only the lessons in English language, <br>
-                        but also the theory book will be in English, and on the exam Vincent will be the translator. <br>
-                        Dont hesitate to call  Vincent on 0634069716 for more information about English lessons <br>
-                        from start to finish, Or advice if you had a license in a foreign country and <br>
-                        are planning to get a Dutch driving license.</p>
-                </div>
+//Als jouw public folder niet te zien is als je naar http://localhost gaat stel dan het juiste basePath in (pas dit pad aan naar jouw situatie)
+$router->setBasePath($CONFIG['BASE_URL']);
 
-                <div class="info__blok2">
-                        <h2>VERDER NOG WAT PLUSPUNTEN OP EEN RIJTJE</h2> 
-                        <ul>
-                            <li>Theorie spoedcursus mogelijk met aansluitend theorie examen bij het CBR.</li>
-                            <li>De rijschool is prima verzekerd en maakt gebruikt van goed onderhouden, veilige en moderne lesauto's.</li>
-                            <li>Al jaren een bovengemiddeld slagingspercentage.</li>
-                            <li>Lessen 7 dagen per week van 8:00 - 21:00 (op vrijdag t/m zondag afwijkende tijden).</li>
-                            <li>Ook voor opfrislessen of 2todrive begeleiders.</li>
-                            <li>Ben je tevreden, vertel het anderen. Ben je niet tevreden, vertel het ons!</li>
-                            <li>Wij gaan zorgvuldig om met jouw persoonsgegevens.</li>
-                        </ul>
-                </div>
-                
-                <div class="info__blok3">
-                        <h2>CONTACT</h2> 
-                        <p>Vragen of meer informatie? Bel of mail ons (bij geen gehoor a.u.b. voicemail inspreken)</p>
-                        <ul>
-                            <li>Krommenie</li>
-                            <li>06 - 26881416</li>
-                            <li>Info@stalrijopleiding.nl</li>
-                            <li>KvK-nummer: 54198437</li>
-                        </ul> 
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<?php include 'includes/footer.php'; ?>
-</body> 
-</html>
+/**
+ * Hier stellen we de juiste "routes" in voor onze website
+ * We vertellen de router welke url naar welk stukje code (de controller) moet worden doorgestuuurd.
+ */
+
+$router->map( 'GET', '/', 'HomeController#homepage', 'home' );
+$router->map( 'GET', '/opleiding', 'OpleidingController#overview', 'opleiding-overzicht' );
+$router->map( 'GET', '/pakketten', 'PackageController#overview', 'pakketten-overzicht' );
+$router->map( 'GET', '/drive', 'DriveController#overview', 'drive-overzicht' );
+$router->map( 'GET', '/links', 'LinksController#overview', 'links-overzicht' );
+$router->map( 'GET', '/contact', 'ContactController#overview', 'contact-overzicht' );
+$router->map( 'GET', '/bestel', 'BestelController#overview', 'bestel-overzicht' );
+// Daarna vragen we $router of de huidige URL getmatcht kan worden.
+$match = $router->match();
+
+/**
+ * Als er een "match" is dan roepen we de controller en de juiste method aan die we zelf hebben ingesteld
+ * Je krijgt namelijk alle info terug in de $match variabele die je nodig hebt om de juiste code aan te roepen
+ * Lees in de documentatie hoe je dit allemaal kunt doen met AltoRouter
+ */
+if ( is_array( $match ) && is_callable( $match['target'] ) ) {
+	//Als het een inline function is roepen we deze meteen aan
+	call_user_func_array( $match['target'], $match['params'] );
+
+} else if ( $match !== false ) {
+	//Anders hakken we de controller#method doormidden op het "#" teken en zetten we ze in twee variabelen
+	list( $controller_name, $method ) = explode( '#', $match['target'] );
+
+	try {
+		// We maken een nieuwe "instance" aan van de juiste controller class
+		$controller = new $controller_name;
+
+		// We roepen we juiste method aan in de controller class
+		call_user_func_array( [ $controller, $method ], $match['params'] );
+
+	} catch ( \Exception $e ) {
+		echo $e->getMessage();
+		exit;
+	}
+} else {
+	// Er is geen match dus een 404 pagina
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found' );
+	echo '404: Onbekende pagina';
+}
