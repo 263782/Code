16cbcc16ccb3c397137fbad9064716ad19fc23d9
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-06-13 15:14:25 +0200

    finneshed repsoinsive on opleiding.php

--- private/views/opleiding.php
+++ private/views/opleiding.php
@@ -4,7 +4,11 @@
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo url('/assets/css/opleiding.css')?>" />
 <?php $this->stop('css')?>
 
-<div class="theorie">
+<?php $this->start('title')?>
+<title>Stal Rijopleiding | Opleiding</title>
+<?php $this->stop('title')?>
+
+    <div class="theorie">
         <h1>DE OPLEIDING</h1>
         <h2>THEORIE</h2>
         <p>Het is handig als je aan het begin van de rijopleiding onmiddellijk start met het leren en oefenen <br>
@@ -26,7 +30,7 @@
         We leren <i>rotondes, erven, spoorwegovergangen</i>, maar ook de snelweg nemen <br>
         we erbij. Je moet leren <i>invoegen, uitrijden, rijstrook wisselen etc</i>. <br>
         Er zit dus duidelijk een opbouw in van makkelijk naar moeilijk! </p>
-    </div>
+    </div> 
     
     <div class="praktijktwee">
         <p>Dit geldt niet alleen voor de gehele cursus, maar ook voor een aparte les. <br>
@@ -39,6 +43,31 @@
         hoe ver je al bent met de opleiding. Hier kan je ook een vrijstelling halen voor het rijexamen m.b.t <br>
         de bijzondere verrichtingen! Na afloop weet je precies welke punten nog aandacht nodig hebben. In het <br>
         laatste deel van de opleiding kunnen we daar nog extra aan gaan werken. Tot slot komt het afsluitende <br>
+        rijexamen bij het <b>CBR</b> en kun je daarna zelfstandig de weg op!
+        </p>
+    </div>
+    
+    <div class="textmobile">
+    <h2>PRAKTIJK</h2> <br>
+        <p>Je gaat starten met je autorijlessen voor rijbewijs categorie B. In een les van 60 of 90 minuten
+        leer je steeds nieuwe vaardigheden. In het begin van de opleiding gaat het vooral <br>
+        om de bediening en beheersing van de auto. Dit moet door veel te oefenen een automatisme gaan worden. <br>
+        Zo heb je tenslotte ook leren fietsen! Daarna kun je steeds meer je aandacht verleggen naar hetgeen wat op de weg gebeurt. <br> <br>
+        We komen bij het 2e gedeelte van de opleiding: De beheersing van eenvoudige verkeersmanoeuvres en verkeerssituaties. <br>
+        Bijvoorbeeld <i>kruispunten, afslaan, achteruitrijden, parkeren, keren</i>. Als dit goed gaat gaan we het uitbreiden naar meer complexe situaties. <br>
+        We leren <i>rotondes, erven, spoorwegovergangen</i>, maar ook de snelweg nemen <br>
+        we erbij. Je moet leren <i>invoegen, uitrijden, rijstrook wisselen etc</i>. <br>
+        Er zit dus duidelijk een opbouw in van makkelijk naar moeilijk! </p> <br>
+        <p>Dit geldt niet alleen voor de gehele cursus, maar ook voor een aparte les. <br>
+        Je krijgt eerst instructie, daarna ga je oefenen en bouwen we de moeilijkheid steeds meer op. <br>
+        Bijvoorbeeld eerst een kruispunt oversteken in een rustig gebied en aan het eind van de les ga je een complex kruispunt <br>
+        oversteken met bijvoorbeeld voetgangersoversteekplaatsen en fiets/bromfietspaden erbij. <br> <br>
+        Jouw vorderingen houden wij bij op een instructiekaart. Zo houden we steeds overzicht over het <br>
+        verloop van de opleiding.Het is ook mogelijk nog een Tussen Tijdse Toets aan te vragen bij het <b>CBR</b> 
+        (<b>C</b>entraal <b>B</b>ureau <b>R</b>ijvaardigheidsbewijzen). <br> <br> Je gaat dan, net zoals een "echt" rijexamen, laten zien <br>
+        hoe ver je al bent met de opleiding. Hier kan je ook een vrijstelling halen voor het rijexamen m.b.t <br>
+        de bijzondere verrichtingen! Na afloop weet je precies welke punten nog aandacht nodig hebben. <br>
+        In het laatste deel van de opleiding kunnen we daar nog extra aan gaan werken. Tot slot komt het afsluitende <br>
         rijexamen bij het <b>CBR</b> en kun je daarna zelfstandig de weg op!
         </p>
     </div>
