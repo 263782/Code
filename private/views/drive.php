b8defd1d61c38cbf39a6c43b415a339997379f35
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-06-14 11:40:00 +0200

    finnished responsive 2 to drive.php

--- private/views/drive.php
+++ private/views/drive.php
@@ -15,13 +15,14 @@
         Daarna mag je zelfstandig de weg op. Op je 16e kan je al theorie-examen doen. <br> <br>
         Ook bij Stal Rijopleiding kun je terecht voor <b>2toDrive</b>. Ook voor een les voor toekomstige begeleiders! <br> <br>
         Meer informatie of heb je vragen? Neem gerust contact op!</p> 
-        <img src="assets/images/2todrive.png" alt="2todrive">
+        <img class="img1" src="assets/images/2todrive.png" alt="2todrive">
     <div class="todrive_info">
         <h2>OVERIGE INFORMATIE</h2>
+        <img class="img2" src="assets/images/2todrive.png" alt="2todrive">
             <p>Wel zo handig toch? <b>2toDrive</b> ingevoerd om de verkeersveiligheid te vergroten. <br>
             Sinds de start hebben al veel jongeren vervroegd hun rijbewijs gehaald. <br>
             Beginnende bestuurders zijn door minder rijervaring vaker betrokken bij ongelukken dan ervaren bestuurders. <br>
             <b>2toDrive</b> zorgt ervoor dat jij onder relatief veilige omstandigheden rijervaring kunt opdoen. <br>
             Zo word jij een zelfverzekerdere bestuurder en heb je minder kans op brokken in het verkeer.</p>
-        </div>
     </div>
+</div> 
